eef2c8930e492c1ace4109d0b1cb7139c2015185
Zachary Durr <zdurr22@gmail.com>
2020-11-21 01:05:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,17 +8,17 @@ class Triangle
   end
 
   def kind
-    if length_a == length_b || length_b == length_c || length_a == length_c
-      @type = :isosceles
-    elsif length_a == length_b && length_a == length_c
+    if length_a == length_b && length_a == length_c
       @type = :equilateral
+    elsif length_a == length_b || length_b == length_c || length_a == length_c
+      @type = :isosceles
     elsif length_a + length_b < length_c || length_b + length_c < length_a || length_a + length_c < length_b
       raise TriangleError
     else
       @type = :scalene
     end
   end
-  
+
 
 
 
